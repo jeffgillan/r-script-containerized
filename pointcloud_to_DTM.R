fbf57c080b7e256ecf5600615da4113597010fe3
Jeffrey K Gillan <jgillan@arizona.edu>
2023-11-03 09:01:04 -0700

    Update pointcloud_to_DTM.R

--- pointcloud_to_DTM.R
+++ pointcloud_to_DTM.R
@@ -8,16 +8,13 @@ library(sp)
 setwd("/home/rstudio/data")
 
 #Bring point cloud into our environment
-point_cloud = readLAS("hole17_point_cloud.laz")
+tree_pointcloud = readLAS("tree.laz")
 
-#Ground filter using cloth simulation filter
-ground = classify_ground(point_cloud,  algorithm = csf(sloop_smooth = FALSE, class_threshold = 0.2, cloth_resolution =  0.3, rigidness = 3))
+#Create a canopy height model (CHM) from the 3D points. Resolution of 10 cm. 
+CHM = rasterize_canopy(tree_pointcloud, res = 0.1, algorithm = p2r(), pkg = "raster")
 
-#Make a point cloud with only ground points
-ground_points = filter_poi(ground, Classification == 2)
-
-#Create a digital terrain model (DTM) from the ground points. Resolution of 10 cm. 
-DTM = grid_terrain(ground_points, res = 0.1, algorithm = knnidw(k = 10, p = 2))
+# Plot the 2D raster CHM
+plot(CHM)
 
 #Write the the raster DTM out to the mounted volume on your local machine
-writeRaster(DTM, filename="DTM_test.tif", format="GTiff", datatype='FLT4S', overwrite=TRUE)
+writeRaster(DTM, filename="CHM.tif", format="GTiff", datatype='FLT4S', overwrite=TRUE)
